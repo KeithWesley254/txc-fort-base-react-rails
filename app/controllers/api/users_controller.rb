75558c623a2154402cc0b6e075a5c6ddc50ebd99
Keith <keithwesley187@gmail.com>
2022-10-07 22:38:17 +0300

    milestone: error handling application controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,15 @@
 class Api::UsersController < ApplicationController
+
+    skip_before_action :authorize, only: [:create]
+    
+    def create
+        user = User.find_by!(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+        end
+    end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+
+    rescue_from ActiveRecord::RecordInvalid, with: :entity_unread
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    before_action :authorize
+
+    private
+
+    def authorize
+        @current_user = User.find_by(id: session[:user_id])
+        render json: { errors: ["Please Login to access this feature"] }, status: :unauthorized unless @current_user
+    end
+
+    def entity_unread(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def render_not_found_response
+        render json: { error: "Oops! Item Not Found" }, status: :not_found
+    end
 end
