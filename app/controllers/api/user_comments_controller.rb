a5e90cf53c74f91ba2598cf339eb149d536c8a61
Keith <keithwesley187@gmail.com>
2023-01-19 02:07:07 +0300

    milestone: jwt focus

--- app/controllers/api/user_comments_controller.rb
+++ app/controllers/api/user_comments_controller.rb
@@ -1,5 +1,7 @@
 class Api::UserCommentsController < ApplicationController
 
+    skip_before_action :authorize, only: [:index, :show]
+
     def index
         slides = UserComment.all
         render json: slides
