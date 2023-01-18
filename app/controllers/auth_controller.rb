96a4b63b1b62f63d17d6c065c58422a39cf61253
Keith <keithwesley187@gmail.com>
2023-01-19 00:59:07 +0300

    milestone: jwt focus

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,5 @@
 class AuthController < ApplicationController
-    skip_before_action :authorized, only: [:create, :auto_login]
+    skip_before_action :authorize, only: [:create, :auto_login]
 
     def create
       @user = User.find_by(email: user_login_params[:email])
